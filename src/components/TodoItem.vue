be2a335ffc9b012e05b96357c281f82849b198d3
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-02-06 09:12:01 -0300

    🎨 Alteração estrutura de item completo
    Utilização do campo data, alteração nas funções para que sejam independentes e é possível marcar tarefa como feita

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,13 @@
   <div class="bg-gray-300 rounded-sm">
     <div class="flex items-center px-4 py-3 border-b border-gray-400 last:border-b-0">
       <div class="flex items-center justify-center mr-2">
-        <button class="text-gray-400">
+        <button
+          :class="{
+            'text-green-600': isCompleted,
+            'text-gray-400': !isCompleted,
+          }"
+          @click="onCheckClick"
+        >
           <svg
             class="w-5 h-5"
             fill="none"
@@ -24,9 +30,9 @@
         <input
           type="text"
           placeholder="Digite a sua tarefa"
-          :value="todo.title"
+          v-model="title"
           class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3"
-          @keyup.enter="updateTodo"
+          @keyup.enter="onTitleChange"
         />
       </div>
 
@@ -63,22 +69,37 @@ export default {
       default: () => ({}),
     },
   },
-  methods: {
-    updateTodo($event) {
-      const newTitle = $event.target.value;
 
-      if (!newTitle) {
+  data() {
+    return {
+      title: this.todo.title,
+      isCompleted: this.todo.completed,
+    };
+  },
+
+  methods: {
+    onTitleChange() {
+      if (!this.title) {
         return;
       }
 
+      this.updateTodo();
+    },
+
+    updateTodo() {
       const payload = {
         id: this.todo.id,
         data: {
-          title: newTitle,
-          completed: this.todo.completed,
+          title: this.title,
+          completed: this.isCompleted,
         },
       };
       this.$store.dispatch("updateTodo", payload);
+    },
+
+    onCheckClick() {
+      this.isCompleted = !this.isCompleted;
+      this.updateTodo();
     },
   },
 };
