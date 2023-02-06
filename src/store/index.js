a9db7a7b69d3691ebcbb0407a2e5268983a9a535
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-02-06 11:03:37 -0300

    ✨ Deleção de tarefa
    Agora se é possível deletar as tarefas da lista de tarefas

--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,13 @@ export default createStore({
       } else {
         state.todos.push(payload);
       }
+    },
+    deleteTodo(state, id) {
+      const index = state.todos.findIndex(todo => todo.id === id);
+
+      if(index >= 0) {
+        state.todos.splice(index, 1);
+      }
     }
   },
   actions: {
@@ -31,10 +38,16 @@ export default createStore({
           commit('storeTodo', response.data);
         })
     },
-    updateTodo({ commit }, { id, data}) {
+    updateTodo({ commit }, { id, data }) {
       return axios.put(`http://localhost:3000/todos/${id}`, data)
               .then((response) => {
                 commit('storeTodo', response.data);
+              })
+    },
+    deleteTodo({ commit }, id) {
+      return axios.delete(`http://localhost:3000/todos/${id}`)
+              .then(() => {
+                commit('deleteTodo', id);
               })
     }
   },
