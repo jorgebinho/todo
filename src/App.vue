577e558eb4a19ab92248f934a749f888478da82f
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-02-06 11:12:40 -0300

    ⚡️ Atualização componente lista vazia
    Agora o componente de lista vazia apenas aparece quando a lista de fato está vazia

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,47 @@
   <!-- Content -->
   <div class="px-3 py-10 md:px-10">
     <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-        <TodoSpinner v-if="loading"/>
+      <TodoSpinner v-if="loading" />
 
-        <template v-else>
-          <TodoFormAdd />
+      <template v-else>
+        <TodoFormAdd />
 
-          <TodoItems />
+        <TodoItems v-if="$store.state.todos.length" />
 
-          <TodoEmpty />
-        </template>
+        <TodoEmpty v-else />
+      </template>
     </div>
   </div>
   <!--/ Content -->
 </template>
 
 <script>
-import TodoSpinner from './components/TodoSpinner.vue';
-import TodoFormAdd from './components/TodoFormAdd.vue';
-import TodoItems from './components/TodoItems.vue';
-import TodoEmpty from './components/TodoEmpty.vue';
+import TodoSpinner from "./components/TodoSpinner.vue";
+import TodoFormAdd from "./components/TodoFormAdd.vue";
+import TodoItems from "./components/TodoItems.vue";
+import TodoEmpty from "./components/TodoEmpty.vue";
 
 export default {
-    name: "App",
+  name: "App",
 
-    data() {
-      return {
-        loading: false
-      }
-    },
+  data() {
+    return {
+      loading: false,
+    };
+  },
 
-    components: { 
-      TodoSpinner, 
-      TodoFormAdd, 
-      TodoItems, 
-      TodoEmpty 
-    },
-    
-    created() {
-      this.loading = true;
-      this.$store.dispatch('getTodos');
-      this.loading = false;
-    }
+  components: {
+    TodoSpinner,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty,
+  },
+
+  created() {
+    this.loading = true;
+    this.$store.dispatch("getTodos");
+    this.loading = false;
+  },
 };
 </script>
 
